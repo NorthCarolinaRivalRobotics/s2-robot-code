5bdc29ea4772000cfb126eb0f63fa8e485669f1e
Ben Caunt <ben@warmhub.com>
2025-08-27 19:55:54 -0400

    standaalone state

--- standalone_drivetrain.py
+++ standalone_drivetrain.py
@@ -11,7 +11,7 @@ import math
 import sys
 import os
 
-from tide import CmdTopic, SensorTopic, StateTopic, robot_topic
+from tide import CmdTopic, StateTopic, robot_topic
 
 # Import Tide models and serialization
 try:
@@ -51,7 +51,7 @@ class StandaloneDriveController:
         # Topic names
         self.cmd_twist_topic = robot_topic(self.robot_id, CmdTopic.TWIST.value)
         self.state_twist_topic = robot_topic(self.robot_id, StateTopic.TWIST.value)
-        self.gyro_topic = robot_topic(self.robot_id, SensorTopic.GYRO_VEL.value)
+        self.gyro_topic = robot_topic(self.robot_id, "sensor/imu/gyro")
         
         # State tracking - use Tide models
         self.current_twist = Twist2D(
