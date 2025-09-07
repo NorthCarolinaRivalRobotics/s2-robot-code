27369186286d2dcc9c4589cd087fac59608e94a9
Ben Caunt <ben@warmhub.com>
2025-09-07 16:05:17 -0400

    adafruit example

--- adafruit_Pca_example.py
+++ adafruit_Pca_example.py
@@ -1,59 +1,57 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 import time
-import zenoh
-import Adafruit_PCA9685
-import json
-from constants import MOTOR_PORTS, MOTOR_REVERSED, SERVO_MIN, SERVO_MAX, SERVO_NEUTRAL
 
-class DriveBase:
-    def __init__(self, address=0x40, busnum=1):
-        self.pwm = Adafruit_PCA9685.PCA9685(address=address, busnum=busnum)
-        self.pwm.set_pwm_freq(60)
-        
-        # Motor configuration
-        self.SERVO_MIN = SERVO_MIN
-        self.SERVO_MAX = SERVO_MAX
-        self.SERVO_NEUTRAL = SERVO_NEUTRAL
-        
-        # Motor ports and reversals
-        self.MOTOR_PORTS = MOTOR_PORTS
-        self.MOTOR_REVERSED = MOTOR_REVERSED
-        
-    def set_motor(self, port, speed):
-        """
-        Set motor speed (-1.0 to 1.0)
-        """
-        # Reverse speed if motor is configured as reversed
-        for motor_name, motor_port in self.MOTOR_PORTS.items():
-            if motor_port == port and self.MOTOR_REVERSED[motor_name]:
-                speed = -speed
-        
-        # Convert -1.0 to 1.0 range to servo pulse range
-        pulse = int(self.SERVO_NEUTRAL + (speed * (self.SERVO_MAX - self.SERVO_MIN) / 2))
-        pulse = max(self.SERVO_MIN, min(self.SERVO_MAX, pulse))
-        self.pwm.set_pwm(port, 0, pulse)
-        print(f"Set motor {port} to {pulse}")
-    
-    def drive(self, x, theta):
-        """
-        Drive using arcade drive
-        x: forward/backward (-1.0 to 1.0)
-        theta: rotation (-1.0 to 1.0)
-        """
-        left = x - theta
-        right = x + theta
-        
-        # Normalize speeds if they exceed [-1, 1]
-        max_magnitude = max(abs(left), abs(right))
-        if max_magnitude > 1.0:
-            left /= max_magnitude
-            right /= max_magnitude
+import board
+from adafruit_motor import servo
 
-        # clip to -0.5 to 0.5
-        left = max(-0.25, min(0.25, left))
-        right = max(-0.25, min(0.25, right))
+from adafruit_pca9685 import PCA9685
 
-        # Set motor speeds
-        self.set_motor(self.MOTOR_PORTS['front_left'], left)
-        self.set_motor(self.MOTOR_PORTS['back_left'], left)
-        self.set_motor(self.MOTOR_PORTS['front_right'], right)
-        self.set_motor(self.MOTOR_PORTS['back_right'], right)
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = busio.I2C(board.GP1, board.GP0)    # Pi Pico RP2040
+
+# Create a simple PCA9685 class instance.
+pca = PCA9685(i2c)
+# You can optionally provide a finer tuned reference clock speed to improve the accuracy of the
+# timing pulses. This calibration will be specific to each board and its environment. See the
+# calibration.py example in the PCA9685 driver.
+# pca = PCA9685(i2c, reference_clock_speed=25630710)
+pca.frequency = 50
+
+# To get the full range of the servo you will likely need to adjust the min_pulse and max_pulse to
+# match the stall points of the servo.
+# This is an example for the Sub-micro servo: https://www.adafruit.com/product/2201
+# servo7 = servo.Servo(pca.channels[7], min_pulse=580, max_pulse=2350)
+# This is an example for the Micro Servo - High Powered, High Torque Metal Gear:
+#   https://www.adafruit.com/product/2307
+# servo7 = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2600)
+# This is an example for the Standard servo - TowerPro SG-5010 - 5010:
+#   https://www.adafruit.com/product/155
+# servo7 = servo.Servo(pca.channels[7], min_pulse=400, max_pulse=2400)
+# This is an example for the Analog Feedback Servo: https://www.adafruit.com/product/1404
+# servo7 = servo.Servo(pca.channels[7], min_pulse=600, max_pulse=2500)
+# This is an example for the Micro servo - TowerPro SG-92R: https://www.adafruit.com/product/169
+# servo7 = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2400)
+
+# The pulse range is 750 - 2250 by default. This range typically gives 135 degrees of
+# range, but the default is to use 180 degrees. You can specify the expected range if you wish:
+# servo7 = servo.Servo(pca.channels[7], actuation_range=135)
+servo7 = servo.Servo(pca.channels[0])
+
+# We sleep in the loops to give the servo time to move into position.
+for i in range(180):
+    servo7.angle = i
+    time.sleep(0.03)
+for i in range(180):
+    servo7.angle = 180 - i
+    time.sleep(0.03)
+
+# You can also specify the movement fractionally.
+fraction = 0.0
+while fraction < 1.0:
+    servo7.fraction = fraction
+    fraction += 0.01
+    time.sleep(0.03)
+
+pca.deinit()
